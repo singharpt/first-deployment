8645dd1a7bd430de26466b1fe6adab24e1fc2495
singharpt <104125829+singharpt@users.noreply.github.com>
2023-11-12 19:14:50 -0600

    added new route for user trips

--- server/routes/users_trips.js
+++ server/routes/users_trips.js
@@ -1,10 +1,11 @@
-import express from 'express'
-import UsersTripsController from '../controllers/users_trips.js'
+import express from "express";
+import UsersTripsController from "../controllers/users_trips.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/create/:trip_id', UsersTripsController.createTripUser)
-router.get('/users/:trip_id', UsersTripsController.getTripUsers)
-router.get('/trips/:username', UsersTripsController.getUserTrips)
- 
-export default router
+router.post("/create/:trip_id", UsersTripsController.createTripUser);
+router.get("/users/:trip_id", UsersTripsController.getTripUsers);
+router.get("/trips/:username", UsersTripsController.getUserTrips);
+router.get("/trips/:userId", UsersTripsController.getUserTrips);
+
+export default router;
